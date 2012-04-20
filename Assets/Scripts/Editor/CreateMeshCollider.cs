e12bf87c597fe13121fa6e30f55b618cd6f352f3
Cristian Ritter <cristiansrguitar@hotmail.com>
2012-04-20 18:48:50 -0300

    Enviando Scripts

---
+++ Assets/Scripts/Editor/CreateMeshCollider.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class CreateMeshCollider : ScriptableObject {
+	[MenuItem ("FrameworkUnity/Colliders/AddMeshCollider")]
+    static void AddMeshCollider() {
+	    foreach (Transform transform in Selection.transforms) {
+			MeshFilter[] meshs = transform.GetComponentsInChildren<MeshFilter>();
+			foreach (MeshFilter mesh in meshs) {
+				mesh.gameObject.AddComponent<MeshCollider>();
+				mesh.GetComponent<MeshCollider>().sharedMesh = mesh.sharedMesh;
+			}
+	    }
+    }
+	
+	[MenuItem ("FrameworkUnity/Colliders/DeleteMeshCollider")]
+    static void DeleteMeshCollider() {
+	    foreach (Transform transform in Selection.transforms) {
+			MeshCollider[] colliders = transform.GetComponentsInChildren<MeshCollider>();
+			foreach (MeshCollider col in colliders) {
+				DestroyImmediate(col);
+			}
+	    }
+    }
+	
+	[MenuItem ("FrameworkUnity/Colliders/AddBoxCollider")]
+    static void AddBoxCollider() {
+	    foreach (Transform transform in Selection.transforms) {
+			MeshFilter[] meshs = transform.GetComponentsInChildren<MeshFilter>();
+			foreach (MeshFilter mesh in meshs) {
+				mesh.gameObject.AddComponent<BoxCollider>();
+			}
+	    }
+    }
+	
+	[MenuItem ("FrameworkUnity/Colliders/DeleteBoxCollider")]
+    static void DeleteBoxCollider() {
+	    foreach (Transform transform in Selection.transforms) {
+			BoxCollider[] colliders = transform.GetComponentsInChildren<BoxCollider>();
+			foreach (BoxCollider col in colliders) {
+				DestroyImmediate(col);
+			}
+	    }
+    }
+	
+	[MenuItem ("FrameworkUnity/Search")]
+    static void Search() {
+		SceneModeUtility.SearchForType(typeof(AudioSource));
+    }
+}
---
+++ Assets/Scripts/Misc/SelectFloor.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using System.Collections;
+
+public class SelectFloor : MonoBehaviour {
+	
+	public class Floor {
+		public string number;
+		public Vector3 position;
+		
+		public Floor (string number, Vector3 position) {
+			this.number = number;
+			this.position = position;
+		}
+	}
+	
+	public Transform selectFloor;
+	public GameObject player;
+	
+	private Floor[] floors;
+	private CameraFollowPlayer cameraFollowPlayer;
+	
+	// Use this for initialization
+	void Start () {
+//		if (selectFloor != null) {
+//			Debug.LogError("Not attached SelectFloor in script");
+//			return;
+//		}
+		
+		player.SetActiveRecursively(false);
+		
+		cameraFollowPlayer = GetComponent<CameraFollowPlayer>();
+		cameraFollowPlayer.enabled = false;
+		
+		floors = new Floor[selectFloor.childCount];
+		int i = 0;
+		foreach (Transform childFloors in selectFloor.transform) {
+			floors[i] = new Floor(childFloors.name, childFloors.localPosition);
+			i++;
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+//		if (selectFloor != null) {
+//			Debug.LogError("Not attached SelectFloor in script");
+//			return;
+//		}
+		
+		GUILayout.BeginArea(new Rect(10, 10, 200, 1000));
+		foreach (Floor floor in floors) {
+			if (GUILayout.Button(floor.number, GUILayout.Width(40), GUILayout.Height(30))) {
+				Vector3 distance = (cameraFollowPlayer.transform.position - player.transform.position);
+				player.transform.position = floor.position;
+				player.SetActiveRecursively(true);
+				cameraFollowPlayer.enabled = true;
+				cameraFollowPlayer.target = player.transform;
+				cameraFollowPlayer.distance =  (distance - player.transform.position) +
+												player.transform.position;
+				enabled = false;
+			}
+		}
+		GUILayout.EndArea();
+	}
+}
