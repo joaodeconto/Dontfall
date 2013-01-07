98b1d8012e1547494ea65b883b7ceb2dac0b94ba
Ian Fandhrs <ianfandhrs@gmail.com>
2013-01-07 18:47:23 -0200

    Add mendigo Low
    Signed-off-by: Ian Fandhrs <ianfandhrs@gmail.com>

--- Assets/Scripts/IA/CarDamage.cs
+++ Assets/Scripts/IA/CarDamage.cs
@@ -81,6 +81,8 @@ public class CarDamage : MonoBehaviour
        
         	meshfilters[j].mesh.vertices = verts;
         	meshfilters[j].mesh.RecalculateBounds();
+        	meshfilters[j].mesh.RecalculateNormals();
+        	meshfilters[j].mesh.MarkDynamic();
         }
 	} 
 }
